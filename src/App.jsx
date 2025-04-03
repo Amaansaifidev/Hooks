50e448083e23379dacfc0679ecc2f61bef6459f4
amaansafidev <amaansaifidev@gmail.com>
2025-04-03 09:28:38 +0000

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import useString from './hooks/useString.js'
+import { useString } from './hooks/useString.js'
 
 function App() {
   const [name, setname] = useString(); 
